3783484dd5df601670e35864acd4edbe46c34eba
魏超 <weic@choicesoft.com.cn>
2019-06-20 09:46:08 +0800

    add:斐波那契数列

---
+++ src/main/java/com/weichao/exercise/algorithm/EditorialDistance.java
@@ -0,0 +1,11 @@
+package com.weichao.exercise.algorithm;
+
+/**
+ * @author weichao
+ * @Description 字符串编辑距离问题
+ * @date 2019/6/19 17:38
+ */
+public class EditorialDistance {
+
+
+}
---
+++ src/main/java/com/weichao/exercise/algorithm/Fibonacci.java
@@ -0,0 +1,60 @@
+package com.weichao.exercise.algorithm;
+
+/**
+ * @author weichao
+ * @Description 斐波那契数列的几种解法
+ * 首先介绍斐波那契数列，斐波那契数列的排列是：1，1，2，3，5，8，13，21，34，55，89，144……
+ * 在数学上，斐波那契数列以如下被以递归的方法定义：F0=0，F1=1，Fn=Fn-1+Fn-2（n>=2，n∈N*），用文字来说，就是斐波那契数列由 0 和 1 开始，之后的斐波那契数列系数就由之前的两数相加。
+ * @date 2019/6/19 16:43
+ */
+public class Fibonacci {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    /**
+     * 递归解法
+     */
+    public int fibonacciRecursive(int n){
+
+        if(n <= 0){
+            return 1;
+        }
+
+        if(n == 1){
+            return 1;
+        }
+
+        return fibonacciRecursive(n-1) + fibonacciRecursive(n-2);
+    }
+
+    /**
+     * 自顶向下的备忘录法
+     */
+    public int fibonacciMemorandum(int n){
+
+        if(n <= 0){
+            return n;
+        }
+
+        int [] memo = new int[n + 1];
+        for(int i = 0;i <= n;i++){
+            memo[i] = -1;
+        }
+        return fibMemorandum(n,memo);
+    }
+
+    public int fibMemorandum(int n,int [] memo){
+        if(memo[n] != -1){
+            return memo[n];
+        }
+        if(n <= 2){
+            memo[n] = 1;
+        } else {
+            memo[n] = fibMemorandum(n-1,memo) + fibMemorandum(n-2,memo);
+        }
+        return memo[n];
+    }
+}
