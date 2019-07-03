371cb4be6d9e22f8a1542f05325269bef8ffb698
魏超 <weic@choicesoft.com.cn>
2019-07-03 17:55:29 +0800

    add:接雨水问题动态规划解法

--- src/main/java/com/weichao/exercise/algorithm/CatchRainwater.java
+++ src/main/java/com/weichao/exercise/algorithm/CatchRainwater.java
@@ -63,12 +63,43 @@ public class CatchRainwater {
      * 动态规划解法
      * @param height
      * @return
+     * 解法来源：https://leetcode-cn.com/problems/trapping-rain-water/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-w-8/
+     *
+     * 我们注意到，解法二中。对于每一列，我们求它左边最高的墙和右边最高的墙，都是重新遍历一遍所有高度，这里我们可以优化一下。
+     *
+     * 首先用两个数组，max_left [ i ] 代表第 i 列左边最高的墙的高度，max_right [ i ] 代表第 i 列右边最高的墙的高度。（一定要注意下，第 i 列左（右）边最高的墙，是不包括自身的，和 leetcode 上边的讲的有些不同）
+     *
+     * 对于 max_left 我们其实可以这样求。
+     *
+     * max_left [ i ] = Max ( max_left [ i - 1] , height [ i - 1]) 。它前边的墙的左边的最高高度和它前边的墙的高度选一个较大的，就是当前列左边最高的墙了。
+     *
+     * 对于 max_right我们可以这样求。
+     *
+     * max_right[ i ] = Max ( max_right[ i + 1] , height [ i + 1]) 。它后边的墙的右边的最高高度和它后边的墙的高度选一个较大的，就是当前列右边最高的墙了。
+     *
+     * 这样，我们再利用解法二的算法，就不用在 for 循环里每次重新遍历一次求 max_left 和 max_right 了。
+     *
      */
     public int trap2(int[] height) {
+        if(height == null || height.length == 0){
+            return 0;
+        }
 
         int sum = 0;
-        for(int i = 1; i < height.length; i++){
-            sum += total(height,i);
+        int [] maxLeft = new int[height.length];
+        int [] maxRight = new int[height.length];
+        maxLeft[0] = height[0];
+        for(int i = 1; i < height.length-1; i++){
+            maxLeft[i] = Math.max(maxLeft[i-1],height[i-1]);
+        }
+        for(int i = height.length-2; i >= 0; i--){
+            maxRight[i] = Math.max(maxRight[i+1],height[i+1]);
+        }
+        for(int i = 1; i < height.length-1; i++){
+            int min = Math.min(maxLeft[i],maxRight[i]);
+            if(min > height[i]){
+                sum += min - height[i];
+            }
         }
         return sum;
     }
