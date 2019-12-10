adba082ef1d39b542b7ccb3c2d9bd4aecb10cf5d
weichao <weichaocst@126.com>
2019-12-11 00:14:46 +0800

    add:线程死锁例子

--- src/main/java/com/weichao/exercise/concurrency/DeadLockDemo.java
+++ src/main/java/com/weichao/exercise/concurrency/DeadLockDemo.java
@@ -1,5 +1,52 @@
 package com.weichao.exercise.concurrency;
 
+/**
+ * 线程死锁例子
+ */
+public class DeadLockDemo{
+
+    public static void main(String[] args) {
+        Object lock1 = new Object();
+        Object lock2 = new Object();
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (lock1){
+                    try {
+                        Thread.sleep(3000);
+                        System.out.println("Thread1获取到lock1");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (lock2){
+                        System.out.println("Thread1获取到lock2");
+                    }
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (lock2){
+                    try {
+                        Thread.sleep(3000);
+                        System.out.println("Thread2获取到lock2");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (lock1){
+                        System.out.println("Thread2获取到lock1");
+                    }
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+    }
+
+}
 
 
 
