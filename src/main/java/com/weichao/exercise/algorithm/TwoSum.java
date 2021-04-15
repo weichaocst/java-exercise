8052a50739016f441b357aada989a7eb30d41ebe
魏超 <weichaocst@126.com>
2021-04-16 01:27:41 +0800

    add:AddTwoNumbers

--- src/main/java/com/weichao/exercise/algorithm/TwoSum.java
+++ src/main/java/com/weichao/exercise/algorithm/TwoSum.java
@@ -11,40 +11,40 @@ import java.util.Map;
 
 /**
  * 1. 两数之和
- *
+ * <p>
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
- *
+ * <p>
  * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素
  * 示例:
- *
+ * <p>
  * 给定 nums = [2, 7, 11, 15], target = 9
- *
+ * <p>
  * 因为 nums[0] + nums[1] = 2 + 7 = 9
  * 所以返回 [0, 1]
- *
  */
 public class TwoSum {
 
     public static void main(String[] args) {
-        int[] example = {2,7,11,15};
+        int[] example = {3, 3};
 
-        int[] result= twoSum(example,9);
+        int[] result = twoSum(example, 6);
 
-        for(int i = 0;i<result.length;i++){
+        for (int i = 0; i < result.length; i++) {
             System.out.println(result[i]);
         }
     }
+
     public static int[] twoSum(int[] nums, int target) {
 
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i = 0;i<nums.length;i++){
-            map.put(nums[i],i);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
         }
 
-        for(int i = 0;i<nums.length;i++){
+        for (int i = 0; i < nums.length; i++) {
             int num = target - nums[i];
-            if(map.containsKey(num) && map.get(num) != i){
-                return new int[]{i,map.get(num)};
+            if (map.containsKey(num) && map.get(num) != i) {
+                return new int[]{i, map.get(num)};
             }
         }
         throw new IllegalArgumentException("No two sum solution");
@@ -52,13 +52,39 @@ public class TwoSum {
 
     public int[] twoSum2(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i< nums.length; i++) {
-            if(map.containsKey(target - nums[i])) {
-                return new int[] {map.get(target-nums[i]),i};
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
             }
             map.put(nums[i], i);
         }
         throw new IllegalArgumentException("No two sum solution");
+    }
+
+
+    /**
+     * 一开始错误的解法，map中相同的元素会覆盖
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSumError(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        map.entrySet().forEach(entry -> {
+            Integer other = target - entry.getKey();
+            if (map.containsKey(other) && !(map.get(other).equals(entry.getValue()))) {
+                result[0] = entry.getValue();
+                result[1] = map.get(other);
+                return;
+            }
+        });
+        return result;
     }
 
 }
