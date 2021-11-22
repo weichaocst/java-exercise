8b4727e07269ddd1f47d8c8879f4aec71b9ed6b4
魏超 <wc244637@alibaba-inc.com>
2021-11-22 23:27:09 +0800

    add:有效的括号

---
+++ src/main/java/com/weichao/exercise/algorithm/ValidParentheses.java
@@ -0,0 +1,43 @@
+package com.weichao.exercise.algorithm;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * 20. 有效的括号
+ * https://leetcode-cn.com/problems/valid-parentheses/submissions/
+ * @author weichao
+ */
+public class ValidParentheses {
+    public static void main(String[] args) {
+        System.out.println(isValid("["));
+    }
+
+    private static final Map<String, String> leftMap = new HashMap<String, String>(){{
+        put("(", ")");
+        put("{", "}");
+        put("[", "]");
+    }};
+
+    public static boolean isValid(String s) {
+        char[] array = s.toCharArray();
+        Stack<String> stringStack = new Stack<>();
+        for (char c : array) {
+            String cs = String.valueOf(c);
+            if (leftMap.containsKey(cs)) {
+                stringStack.push(cs);
+            } else {
+                if (!stringStack.empty() && cs.equals(leftMap.get(stringStack.peek()))) {
+                    stringStack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        if (stringStack.empty()) {
+            return true;
+        }
+        return false;
+    }
+}
