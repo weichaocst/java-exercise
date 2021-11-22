abc847a5e134c884837a8dd91a9343badd7d4959
魏超 <wc244637@alibaba-inc.com>
2021-11-22 23:29:54 +0800

    add:有效的括号代码优化

--- src/main/java/com/weichao/exercise/algorithm/ValidParentheses.java
+++ src/main/java/com/weichao/exercise/algorithm/ValidParentheses.java
@@ -35,9 +35,6 @@ public class ValidParentheses {
                 }
             }
         }
-        if (stringStack.empty()) {
-            return true;
-        }
-        return false;
+       return stringStack.empty();
     }
 }
